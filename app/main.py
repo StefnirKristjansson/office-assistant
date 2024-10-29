99f98ca663c0e451a12822c28cc9832b093b0a3e
Stefnirk <stefnir@proency.com>
2024-10-29 12:55:54 +0000

    Refactor main.py and requirements.txt
    Remove unused import and route for authentication in main.py
    Add Jinja2 to requirements.txt

--- app/main.py
+++ app/main.py
@@ -6,13 +6,11 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routes import auth
 from app.routes.index import router as index_router
 
 load_dotenv()
 
 app = FastAPI()
-app.include_router(auth.router)
 app.include_router(index_router)
 
 
